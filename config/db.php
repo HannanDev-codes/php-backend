1f5a0e89b45f251bb5bd993c71363bcc4bd044f5
HannanDev-codes <abdulhannankhalid10@gmail.com>
2026-05-15 16:42:47 +0500

    Add config folder with db.php

--- config/db.php
+++ config/db.php
@@ -1,6 +1,5 @@
-<?php
+echo ^<?php
 $database_url = getenv('DATABASE_URL');
-
 if ($database_url) {
     $db_config = parse_url($database_url);
     $host = $db_config['host'];
@@ -11,15 +10,14 @@ if ($database_url) {
 } else {
     $host = 'localhost';
     $port = 3306;
-    $dbname = 'your_database';
+    $dbname = 'railway';
     $username = 'root';
     $password = '';
 }
-
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    die(json_encode(["success" => false, "message" => "DB Connection Failed: " . $e->getMessage()]));
+    die(json_encode(['success' => false, 'message' => 'DB Connection Failed: ' . $e->getMessage()]));
 }
-?>
+?^> > config/db.php
